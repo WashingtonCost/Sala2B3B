ec940a84b407902f2664db454d1f7642ed786b5c
WashingtonCost <126517167+WashingtonCost@users.noreply.github.com>
2023-05-08 15:57:19 -0400

    atualização

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 function abrirMenu(){
     const divMenu = document.getElementById('respMenu').classList;
     divMenu.toggle('abrir');
+    const ic = document.getElementById('icone');
+    if(divMenu.contains('abrir')){
+        ic.innerHTML = 'close';
+    
+    }else{
+        ic.innerHTML = 'menu';
+    }
 }
